0b97b0de3aee558f1d3b14c968837004f4ba3fb7
Alistair Pullen <herri98@gmail.com>
2023-05-04 12:38:47 -0700

    Update gitignore

--- src/scratchpad.ts
+++
@@ -1,9 +0,0 @@
-import { HNSW } from "./index";
-import { readFileSync } from "fs";
-
-(async () => {
-    const data = readFileSync('./src/base.embeddings.bin')
-    const index = HNSW.deserialize(data)
-    console.log(index.getSize())
-    console.log(index.getNodes().next().value.vector)
-})()
